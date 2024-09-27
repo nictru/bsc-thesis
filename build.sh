f2355d743c4bc40cddbc428d2b7745c88bf58d08
Nico Trummer <nictru32@gmail.com>
2024-09-27 18:26:26 +0000

    Fix latex build script

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 
 mkdir -p build
 pdflatex -output-directory=build main.tex
+cd build && makeglossaries "main" && cd ..
+pdflatex -output-directory=build main.tex
+
 mv build/main.pdf build/Thesis.pdf
