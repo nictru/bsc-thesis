1e38d12d08a8a39b4de09769dcfaf959101c50a1
Nico Trummer <nictru32@gmail.com>
2024-10-09 20:15:05 +0000

    Remove glossary in favor of in-place long-form

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 mkdir -p build
 pdflatex -output-directory=build main.tex
-cd build && makeglossaries "main" && cd ..
-pdflatex -output-directory=build main.tex
 
 mv build/main.pdf build/Thesis.pdf
